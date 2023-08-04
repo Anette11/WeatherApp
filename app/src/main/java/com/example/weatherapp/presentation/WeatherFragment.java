2a7cbde3485bcef6a0d68ddbbf42bb45b3113580
Anette11 <krivaltseva@gmail.com>
2023-08-04 16:38:48 +0400

    Rewrite set layout in WeatherFragment using viewBinding

--- app/src/main/java/com/example/weatherapp/presentation/WeatherFragment.java
+++ app/src/main/java/com/example/weatherapp/presentation/WeatherFragment.java
@@ -1,23 +1,37 @@
 package com.example.weatherapp.presentation;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.example.weatherapp.R;
+import com.example.weatherapp.databinding.FragmentWeatherBinding;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
 public class WeatherFragment extends Fragment {
 
-    public WeatherFragment() {
-        super(R.layout.fragment_weather);
+    private FragmentWeatherBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState
+    ) {
+        binding = FragmentWeatherBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }

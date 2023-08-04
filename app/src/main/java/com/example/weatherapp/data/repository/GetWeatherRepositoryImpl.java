ff75edf9d8310e301a5b87d4088ea6e4b02178ba
Anette11 <krivaltseva@gmail.com>
2023-08-04 16:59:05 +0400

    Add inject annotation for constructor of GetWeatherRepositoryImpl class

--- app/src/main/java/com/example/weatherapp/data/repository/GetWeatherRepositoryImpl.java
+++ app/src/main/java/com/example/weatherapp/data/repository/GetWeatherRepositoryImpl.java
@@ -5,9 +5,15 @@ import android.database.Observable;
 import com.example.weatherapp.data.remote.GetWeatherResponseDto;
 import com.example.weatherapp.domain.repository.GetWeatherRepository;
 
+import javax.inject.Inject;
+
 import retrofit2.Response;
 
 public class GetWeatherRepositoryImpl implements GetWeatherRepository {
+
+    @Inject
+    public GetWeatherRepositoryImpl() {
+    }
 
     @Override
     public Observable<Response<GetWeatherResponseDto>> getWeather() {

8801f23a2b9d2189480e28a2999702d61242836f
Anette11 <krivaltseva@gmail.com>
2023-08-04 16:13:50 +0400

    Rename from WeatherModule to NetworkModule

--- app/src/main/java/com/example/weatherapp/di/NetworkModule.java
+++ app/src/main/java/com/example/weatherapp/di/NetworkModule.java
@@ -14,7 +14,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
 @InstallIn(SingletonComponent.class)
-public class WeatherModule {
+public class NetworkModule {
 
     @Provides
     @Named("baseUrl")

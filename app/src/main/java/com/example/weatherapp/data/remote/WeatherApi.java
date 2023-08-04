df75f4cbc01cac61a05beaaba6270ee2020f6866
Anette11 <krivaltseva@gmail.com>
2023-08-04 15:25:42 +0400

    Create interface WeatherApi

---
+++ app/src/main/java/com/example/weatherapp/data/remote/WeatherApi.java
@@ -0,0 +1,14 @@
+package com.example.weatherapp.data.remote;
+
+import android.database.Observable;
+
+import retrofit2.Response;
+import retrofit2.http.GET;
+
+interface WeatherApi {
+
+    static final String BASE_URL = "https://api.open-meteo.com/";
+
+    @GET("v1/forecast?hourly=temperature_2m,relativehumidity_2m,weathercode,windspeed_10m")
+    Observable<Response<GetWeatherResponseDto>> getWeather();
+}

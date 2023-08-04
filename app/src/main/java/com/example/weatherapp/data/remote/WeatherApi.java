2e88a8fa86ca1ff4b826a9619bc1e71c9780717e
Anette11 <krivaltseva@gmail.com>
2023-08-04 17:01:34 +0400

    Remove not needed modifiers static final from base url in WeatherApi

--- app/src/main/java/com/example/weatherapp/data/remote/WeatherApi.java
+++ app/src/main/java/com/example/weatherapp/data/remote/WeatherApi.java
@@ -7,7 +7,7 @@ import retrofit2.http.GET;
 
 public interface WeatherApi {
 
-    static final String BASE_URL = "https://api.open-meteo.com/";
+    String BASE_URL = "https://api.open-meteo.com/";
 
     @GET("v1/forecast?hourly=temperature_2m,relativehumidity_2m,weathercode,windspeed_10m")
     Observable<Response<GetWeatherResponseDto>> getWeather();

b39dd2c1561ab16a5be17549f13f107f1ca12d37
Anette11 <krivaltseva@gmail.com>
2023-08-05 14:57:45 +0400

    Reload icons

--- app/src/main/java/com/example/weatherapp/WeatherType.java
+++ app/src/main/java/com/example/weatherapp/WeatherType.java
@@ -6,142 +6,142 @@ public enum WeatherType {
 
     CLEAR_SKY(
             "Clear sky",
-            R.drawable.clear_day,
+            R.drawable.day_sunny_icon,
             0
     ),
     MAINLY_CLEAR(
             "Mainly clear",
-            R.drawable.clear_day,
+            R.drawable.day_sunny_icon,
             1
     ),
     PARTLY_CLEAR(
             "Partly cloudy",
-            R.drawable.cloudy_partly_day,
+            R.drawable.day_cloudy_icon,
             2
     ),
     OVERCAST(
             "Overcast",
-            R.drawable.cloudy,
+            R.drawable.cloud_icon,
             3
     ),
     FOG(
             "Fog",
-            R.drawable.fog,
+            R.drawable.cloud_fog_icon,
             45
     ),
     DEPOSITING_RIME_FOG(
             "Depositing rime fog",
-            R.drawable.fog,
+            R.drawable.cloud_fog_icon,
             48
     ),
     DRIZZLE_LIGHT(
             "Drizzle light",
-            R.drawable.rain_small,
+            R.drawable.cloud_rain_icon,
             51
     ),
     DRIZZLE_MODERATE(
             "Drizzle moderate",
-            R.drawable.rain_small,
+            R.drawable.cloud_rain_icon,
             53
     ),
     DRIZZLE_DENSE(
             "Drizzle dense",
-            R.drawable.rain_small,
+            R.drawable.cloud_rain_icon,
             55
     ),
     FREEZING_DRIZZLE_LIGHT(
             "Freezing drizzle light",
-            R.drawable.snow_grains,
+            R.drawable.cloud_rain_icon,
             56
     ),
     FREEZING_DRIZZLE_DENSE(
             "Freezing drizzle dense",
-            R.drawable.snow_grains,
+            R.drawable.cloud_snow_icon,
             57
     ),
     RAIN_SLIGHT(
             "Rain slight",
-            R.drawable.rain_heavy,
+            R.drawable.cloud_rain_icon,
             61
     ),
     RAIN_MODERATE(
             "Rain moderate",
-            R.drawable.rain_heavy,
+            R.drawable.cloud_rain_icon,
             63
     ),
     RAIN_HEAVY(
             "Rain heavy",
-            R.drawable.rain_heavy,
+            R.drawable.cloud_rain_icon,
             65
     ),
     FREEZING_RAIN_LIGHT(
             "Freezing rain light",
-            R.drawable.snow_grains,
+            R.drawable.cloud_snow_icon,
             66
     ),
     FREEZING_RAIN_HEAVY(
             "Freezing rain heavy",
-            R.drawable.snow_grains,
+            R.drawable.cloud_snow_icon,
             67
     ),
     SNOW_FALL_SLIGHT(
             "Snow fall slight",
-            R.drawable.snow,
+            R.drawable.cloud_snow_icon,
             71
     ),
     SNOW_FALL_MODERATE(
             "Snow fall moderate",
-            R.drawable.snow,
+            R.drawable.cloud_snow_icon,
             73
     ),
     SNOW_FALL_HEAVY(
             "Snow fall heavy",
-            R.drawable.snow,
+            R.drawable.cloud_snow_icon,
             75
     ),
     SNOW_GRAINS(
             "Snow grains",
-            R.drawable.snow_grains,
+            R.drawable.cloud_snow_icon,
             77
     ),
     RAIN_SHOWERS_SLIGHT(
             "Rain showers slight",
-            R.drawable.rain_shower,
+            R.drawable.cloud_rain_icon,
             80
     ),
     RAIN_SHOWERS_MODERATE(
             "Rain showers moderate",
-            R.drawable.rain_shower,
+            R.drawable.cloud_rain_icon,
             81
     ),
     RAIN_SHOWERS_VIOLENT(
             "Rain showers violent",
-            R.drawable.rain_shower,
+            R.drawable.cloud_rain_icon,
             82
     ),
     SNOW_SHOWERS_SLIGHT(
             "Snow showers slight",
-            R.drawable.snow,
+            R.drawable.cloud_snow_icon,
             85
     ),
     SNOW_SHOWERS_HEAVY(
             "Snow showers heavy",
-            R.drawable.snow,
+            R.drawable.cloud_snow_icon,
             86
     ),
     THUNDERSTORM(
             "Thunderstorm slight or moderate",
-            R.drawable.thunderstorm,
+            R.drawable.cloud_lightning_icon,
             95
     ),
     THUNDERSTORM_SLIGHT_HAIL(
             "Thunderstorm with slight hail",
-            R.drawable.thunderstorm_rain,
+            R.drawable.cloud_rain_lightning_icon,
             96
     ),
     THUNDERSTORM_HEAVY_HAIL(
             "Thunderstorm with heavy hail",
-            R.drawable.thunderstorm_rain,
+            R.drawable.cloud_rain_lightning_icon,
             99
     );
 

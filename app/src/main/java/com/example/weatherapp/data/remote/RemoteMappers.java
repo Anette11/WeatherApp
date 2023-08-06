438918f8bbd8da41b39abd1a39b8f7ad779739b8
Anette11 <krivaltseva@gmail.com>
2023-08-06 22:42:17 +0400

    Remove not used variables and methods from GetWeatherResponse

--- app/src/main/java/com/example/weatherapp/data/remote/RemoteMappers.java
+++ app/src/main/java/com/example/weatherapp/data/remote/RemoteMappers.java
@@ -2,27 +2,15 @@ package com.example.weatherapp.data.remote;
 
 import com.example.weatherapp.data.remote.dto.GetWeatherResponseDto;
 import com.example.weatherapp.data.remote.dto.HourlyDto;
-import com.example.weatherapp.data.remote.dto.HourlyUnitsDto;
 import com.example.weatherapp.domain.data.GetWeatherResponse;
 import com.example.weatherapp.domain.data.Hourly;
-import com.example.weatherapp.domain.data.HourlyUnits;
 
 public class RemoteMappers {
 
     public static GetWeatherResponse fromGetWeatherResponseDtoToGetWeatherResponse(
             GetWeatherResponseDto getWeatherResponseDto
     ) {
-        return new GetWeatherResponse(
-                getWeatherResponseDto.getElevation(),
-                getWeatherResponseDto.getGenerationTimeMs(),
-                fromHourlyDtoToHourly(getWeatherResponseDto.getHourly()),
-                fromHourlyUnitsDtoToHourlyUnits(getWeatherResponseDto.getHourlyUnits()),
-                getWeatherResponseDto.getLatitude(),
-                getWeatherResponseDto.getLongitude(),
-                getWeatherResponseDto.getTimezone(),
-                getWeatherResponseDto.getTimezoneAbbreviation(),
-                getWeatherResponseDto.getUtcOffsetSeconds()
-        );
+        return new GetWeatherResponse(fromHourlyDtoToHourly(getWeatherResponseDto.getHourly()));
     }
 
     private static Hourly fromHourlyDtoToHourly(
@@ -34,18 +22,6 @@ public class RemoteMappers {
                 hourlyDto.getTime(),
                 hourlyDto.getWeatherCode(),
                 hourlyDto.getWindSpeed10m()
-        );
-    }
-
-    private static HourlyUnits fromHourlyUnitsDtoToHourlyUnits(
-            HourlyUnitsDto hourlyUnitsDto
-    ) {
-        return new HourlyUnits(
-                hourlyUnitsDto.getRelativeHumidity2m(),
-                hourlyUnitsDto.getTemperature2m(),
-                hourlyUnitsDto.getTime(),
-                hourlyUnitsDto.getWeatherCode(),
-                hourlyUnitsDto.getWindSpeed10m()
         );
     }
 }

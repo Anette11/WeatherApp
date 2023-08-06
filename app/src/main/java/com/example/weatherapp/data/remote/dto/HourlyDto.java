dc45a758bf45aba5e41533b711468931addb6718
Anette11 <krivaltseva@gmail.com>
2023-08-06 22:34:46 +0400

    Remove setters from class HourlyDto

--- app/src/main/java/com/example/weatherapp/data/remote/dto/HourlyDto.java
+++ app/src/main/java/com/example/weatherapp/data/remote/dto/HourlyDto.java
@@ -6,14 +6,14 @@ import java.util.List;
 
 public class HourlyDto {
     @SerializedName("relativehumidity_2m")
-    List<Integer> relativeHumidity2m;
+    private final List<Integer> relativeHumidity2m;
     @SerializedName("temperature_2m")
-    List<Double> temperature2m;
-    List<String> time;
+    private final List<Double> temperature2m;
+    private final List<String> time;
     @SerializedName("weathercode")
-    List<Integer> weatherCode;
+    private final List<Integer> weatherCode;
     @SerializedName("windspeed_10m")
-    List<Double> windSpeed10m;
+    private final List<Double> windSpeed10m;
 
     public HourlyDto(
             List<Integer> relativeHumidity2m,
@@ -33,39 +33,19 @@ public class HourlyDto {
         return relativeHumidity2m;
     }
 
-    public void setRelativeHumidity2m(List<Integer> relativeHumidity2m) {
-        this.relativeHumidity2m = relativeHumidity2m;
-    }
-
     public List<Double> getTemperature2m() {
         return temperature2m;
-    }
-
-    public void setTemperature2m(List<Double> temperature2m) {
-        this.temperature2m = temperature2m;
     }
 
     public List<String> getTime() {
         return time;
     }
 
-    public void setTime(List<String> time) {
-        this.time = time;
-    }
-
     public List<Integer> getWeatherCode() {
         return weatherCode;
     }
 
-    public void setWeatherCode(List<Integer> weatherCode) {
-        this.weatherCode = weatherCode;
-    }
-
     public List<Double> getWindSpeed10m() {
         return windSpeed10m;
-    }
-
-    public void setWindSpeed10m(List<Double> windSpeed10m) {
-        this.windSpeed10m = windSpeed10m;
     }
 }

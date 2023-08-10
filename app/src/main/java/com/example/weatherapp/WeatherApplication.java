5a464ed6a1724f73221d8124243bc7887a2ad8f8
Anette11 <krivaltseva@gmail.com>
2023-08-10 14:36:14 +0400

    Remove empty line from method onCreate() in WeatherApplication

--- app/src/main/java/com/example/weatherapp/WeatherApplication.java
+++ app/src/main/java/com/example/weatherapp/WeatherApplication.java
@@ -11,7 +11,6 @@ public class WeatherApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
         if (BuildConfig.DEBUG) Timber.plant(new Timber.DebugTree());
     }
 }
